2f51401564dfde6e555a5da19824330553709894
Ru49 <hyonjiwoo@gmail.com>
2020-08-13 10:55:26 +0900

    생성자 오버로딩 연습

---
+++ JavaEx/src/com/javaex/oop/goods/v4/GoodApp.java
@@ -0,0 +1,33 @@
+package com.javaex.oop.goods.v4;
+
+// v4.
+// this
+public class GoodApp {
+
+	public static void main(String[] args) {
+		// Goods 클래스 사용
+		
+		
+		// 생성자 활용
+		Goods notebook = new Goods("LG Gram", 1500000);
+		
+		// setter를 이용한 우회 접근
+		/*
+		notebook.setName("LG Gram");
+		notebook.setPrice(1500000);
+		*/
+		
+		// 메서드 호출
+		notebook.showInfo();
+		
+		Goods smartphone = new Goods("iPhone SE", 600000);
+		
+		/*
+		smartphone.setName("iPhone SE");
+		smartphone.setPrice(600000);
+		*/
+		smartphone.showInfo();
+		
+	}
+
+}
