0d3e8e33212864f76fd7ea22dbc3d43b0ce3ec4b
Ru49 <hyonjiwoo@gmail.com>
2020-08-18 12:46:10 +0900

    객체지향 정리

---
+++ JavaEx/src/com/javaex/oop/summary/Panda.java
@@ -0,0 +1,19 @@
+package com.javaex.oop.summary;
+
+public class Panda extends Animal {
+	// 생성자
+	public Panda(String name, int age) {
+		// 부모 생성자 호출
+		super(name, age);
+	}
+
+	@Override
+	public void say() {
+		// abstract 매서드이므로 반드시 오버라이드
+		System.out.println("zzz....");
+		
+	}
+	
+	
+
+}
