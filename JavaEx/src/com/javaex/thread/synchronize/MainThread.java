37a7ca999a7f81ac557df4f3319bb028eaa7544f
Ru49 <hyonjiwoo@gmail.com>
2020-08-26 12:47:03 +0900

    Thread, InetAddress

---
+++ JavaEx/src/com/javaex/thread/synchronize/MainThread.java
@@ -0,0 +1,11 @@
+package com.javaex.thread.synchronize;
+
+
+public class MainThread {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ JavaEx/src/com/javaex/thread/synchronize/ShareMemory.java
@@ -0,0 +1,9 @@
+package com.javaex.thread.synchronize;
+
+//여러 쓰레드에서 공유하는 참조 객체
+public class ShareMemory {
+	private int memory;
+	
+	
+
+}
